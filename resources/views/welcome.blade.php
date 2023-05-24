b5786c2e43dc475c6bc01508c0a552cef18f76c2
Rolly Castillo <rollcastillo010@gmail.com>
2023-05-24 17:54:30 +0800

    On progress sidebar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,145 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="h-screen w-screen bg-red-500">
+@include('partials.header')
+        <header class="bg-slate-800 p-2 flex justify-between">
+            <span>
+                <img class="h-10 w-20" src="img/navlogo.png" alt="Header logo">
+            </span>
+            <div class="relative">
+                <span>
+                    <svg class="h-10 w-10 fill-current text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="24" height="24" fill="none"/>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9999 6C9.79077 6 7.99991 7.79086 7.99991 10C7.99991 12.2091 9.79077 14 11.9999 14C14.209 14 15.9999 12.2091 15.9999 10C15.9999 7.79086 14.209 6 11.9999 6ZM17.1115 15.9974C17.8693 16.4854 17.8323 17.5491 17.1422 18.1288C15.7517 19.2966 13.9581 20 12.0001 20C10.0551 20 8.27215 19.3059 6.88556 18.1518C6.18931 17.5723 6.15242 16.5032 6.91351 16.012C7.15044 15.8591 7.40846 15.7251 7.68849 15.6097C8.81516 15.1452 10.2542 15 12 15C13.7546 15 15.2018 15.1359 16.3314 15.5954C16.6136 15.7102 16.8734 15.8441 17.1115 15.9974Z"/>
+                    </svg>
+                </span>
 
-    </div>
-</body>
-</html>
+                <div class="bg-white py-2 px-4 absolute right-0 w-max rounded divide-y-2 shadow-md">
+                    <div class="py-2 flex flex-col leading-8">
+                        <span>Profile</span>
+                        <span>Update credentials</span>
+                    </div>
+                    <div class="py-2">Signout</div>
+                </div>
+            </div>
+        </header>
+        <div>
+            
+            <aside>
+                <nav>
+                    <div>
+                        <div class="flex">
+                            <span></span>
+                            <span>Asset Monitoring</span>
+                            <span>
+                                <svg class="w-6 h-6" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Arrow / Chevron_Down">
+                                    <path id="Vector" d="M19 9L12 16L5 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                        <ul class="hidden">
+                            <li>New Movement</li>
+                            <li>Hardware</li>
+                            <li>Software</li>
+                            <li>Service Support</li>
+                            <li>Vendor</li>
+                            <li>Toner</li>
+                            <li>Others</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div class="flex">
+                            <span></span>
+                            <span>Department</span>
+                            <span>
+                                <svg class="w-6 h-6" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Arrow / Chevron_Down">
+                                    <path id="Vector" d="M19 9L12 16L5 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                        <ul class="hidden">
+                            <li>New Movement</li>
+                            <li>Hardware</li>
+                            <li>Software</li>
+                            <li>Service Support</li>
+                            <li>Vendor</li>
+                            <li>Toner</li>
+                            <li>Others</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div class="flex">
+                            <span></span>
+                            <span>Employee List</span>
+                            <span>
+                                <svg class="w-6 h-6" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Arrow / Chevron_Down">
+                                    <path id="Vector" d="M19 9L12 16L5 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                        <ul class="hidden">
+                            <li>New Movement</li>
+                            <li>Hardware</li>
+                            <li>Software</li>
+                            <li>Service Support</li>
+                            <li>Vendor</li>
+                            <li>Toner</li>
+                            <li>Others</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div class="flex">
+                            <span></span>
+                            <span>Users</span>
+                            <span>
+                                <svg class="w-6 h-6" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Arrow / Chevron_Down">
+                                    <path id="Vector" d="M19 9L12 16L5 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                        <ul class="hidden">
+                            <li>New Movement</li>
+                            <li>Hardware</li>
+                            <li>Software</li>
+                            <li>Service Support</li>
+                            <li>Vendor</li>
+                            <li>Toner</li>
+                            <li>Others</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <div class="flex">
+                            <span></span>
+                            <span>Reports</span>
+                            <span>
+                                <svg class="w-6 h-6" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Arrow / Chevron_Down">
+                                    <path id="Vector" d="M19 9L12 16L5 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                        <ul class="hidden">
+                            <li>New Movement</li>
+                            <li>Hardware</li>
+                            <li>Software</li>
+                            <li>Service Support</li>
+                            <li>Vendor</li>
+                            <li>Toner</li>
+                            <li>Others</li>
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+            <main></main>
+        </div>
+@include('partials.footer')
